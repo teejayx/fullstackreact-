bbb17bd1999976c4ab44bf47fc3d2d9b0c9a2dad
Olatunji S <teejayx@outlook.com>
2022-05-04 12:39:29 +0100

    display productslist and products

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,89 @@
 class ProductList extends React.Component {
-  render() {
+  handleProductUpVote(productId){
+    console.log(productId + ' was upvoted');
+  }
+
+
+
+  render() { 
+
+    const products = Seed.products.sort((a, b) => (
+       b.votes - a.votes
+
+    ));
+
+    const productComponet = products.map((product) => (
+      <Product
+      key={'product-' + product.id}
+      id = {product.id}
+      title = {product.title}
+      description={product.description}
+      url={product.url}
+      votes={product.votes}
+      submitterAvatarUrl={product.submitterAvatarUrl}
+      productImageUrl={product.productImageUrl}
+      onVote={this.handleProductUpVote}
+      />
+
+  ));
+    
+    
+    
     return (
       <div className='ui unstackable items'>
-        Hello, friend! I am a basic React component.
+        {productComponet}
       </div>
     );
   }
 }
+
+
+
+class Product extends React.Component {
+
+  constructor(props){
+      super(props);
+      this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+
+ handleUpVote(){
+   this.props.onVote(this.props.id);
+ }
+
+  render() {
+    return (
+      <div className='item'>
+      <div className='image'>
+        <img src={this.props.productImageUrl} />
+      </div>
+      <div className='middle aligned content'>
+      <div className='header'>
+      <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
+              </a>
+            {this.props.votes}
+          </div>
+      <div className='description'>
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src={this.props.submitterAvatarUrl}
+            />
+          </div>
+        </div>
+</div>
+
+
+    );
+  }
+}
+
+
 
 ReactDOM.render(
   <ProductList />,
